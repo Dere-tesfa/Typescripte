174f5d39f3f2369392d59dc374e3e38a9257aff0
Dereje Tesfaye <deretesfa6@gmail.com>
2025-08-30 20:04:08 +0300

    create new class

---
+++ src/class/class.ts
@@ -0,0 +1,19 @@
+class Account {
+    //instance 
+    id: number;
+    owner: String;
+    balance: number;
+    // constructer method
+    constructor(id: number, owner: string, balance: number) {
+        //assign 
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    //this is another method
+    deposit(amount: number) {
+        if (amount <= 0) {
+            throw new Error("Invalid")
+        }
+    }
+}
